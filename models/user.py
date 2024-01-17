d6a2d45750aced19f4bd54718057518fb1df8956
elhaddoumi <elhaddoumi99@gmail.com>
2024-01-17 15:56:12 +0100

    0x02. AirBnB clone - MySQL

--- models/user.py
+++ models/user.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-"""User class that inherits from BaseModel"""
-
+"""This module defines a class User"""
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """User class that inherits from BaseModel"""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+class User(BaseModel, Base):
+    """This class defines a user by various attributes"""
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
