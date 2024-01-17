6cfecda4ea079b8598d6f0df491069b1b9320026
elhaddoumi <elhaddoumi99@gmail.com>
2024-01-17 15:55:58 +0100

    0x02. AirBnB clone - MySQL

--- models/state.py
+++ models/state.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
-"""State class"""
-
+""" State Module for HBNB project """
+import models
+from os import getenv
+from models.base_model import Base
 from models.base_model import BaseModel
+from models.city import City
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """state class that inherits from BaseModel"""
-    name = ""
+class State(BaseModel, Base):
+    """ State class """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        # file storage
+        @property
+        def cities(self):
+            """Get a list of all related City Objects"""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
