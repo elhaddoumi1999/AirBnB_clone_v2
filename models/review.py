85da2d97680d661f2f243967211ef19dda2f9d07
elhaddoumi <elhaddoumi99@gmail.com>
2024-01-17 15:55:41 +0100

    0x02. AirBnB clone - MySQL

--- models/review.py
+++ models/review.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-"""Review class"""
-
+""" Review module for the HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
-    """review class that inherits from BaseModel"""
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """ Review class to store review information """
+    __tablename__ = "reviews"
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    text = Column(String(1024), nullable=False)
