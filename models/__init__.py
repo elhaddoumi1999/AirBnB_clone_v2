e8bf396749de08120c7ef17a82e70900cb490e4e
elhaddoumi <elhaddoumi99@gmail.com>
2024-01-17 15:54:18 +0100

    0x02. AirBnB clone - MySQL

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
-""" the __init__ module"""
-from models.engine.file_storage import FileStorage
+"""
+instantiates storage object
+if environmental variable 'HBNB_TYPE_STORAGE' is set to 'db',
+instantiate database storage
+otherwise instantiates FileStorage
+"""
+from os import getenv
 
-storage = FileStorage()
-storage.reload()
+
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
