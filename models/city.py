b6f7d9dd143e12b96ba8c163c5b8a4f3c9ecc1ed
elhaddoumi <elhaddoumi99@gmail.com>
2024-01-17 15:55:12 +0100

    0x02. AirBnB clone - MySQL

--- models/city.py
+++ models/city.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""City class"""
-
+""" City Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """City class that inherits from BaseModel"""
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
